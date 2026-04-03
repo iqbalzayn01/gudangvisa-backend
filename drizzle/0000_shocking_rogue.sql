fa1f2c4a3fc09e3259d60ab94dc87a67d21b9ab9
iqbalzayn01 <iqbalzayyanabiyyu@gmail.com>
2026-04-03 22:25:31 +0700

    update: fix drizzle config

--- drizzle/0000_shocking_rogue.sql
+++ drizzle/0000_shocking_rogue.sql
@@ -3,22 +3,23 @@ CREATE TYPE "public"."doc_type" AS ENUM('VISA', 'KITAS', 'PASSPORT');--> stateme
 CREATE TYPE "public"."role" AS ENUM('STAFF', 'ADMIN');--> statement-breakpoint
 CREATE TABLE "documents" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"resi_number" varchar(255) NOT NULL,
+	"tracking_code" varchar(50) NOT NULL,
 	"client_name" varchar(255) NOT NULL,
-	"type" "doc_type" NOT NULL,
+	"doc_type" "doc_type" NOT NULL,
 	"status" "doc_status" DEFAULT 'RECEIVED' NOT NULL,
-	"file_url" text NOT NULL,
-	"staff_id" uuid NOT NULL,
+	"file_url" text,
+	"created_by" uuid,
 	"created_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "documents_resi_number_unique" UNIQUE("resi_number")
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "documents_tracking_code_unique" UNIQUE("tracking_code")
 );
 --> statement-breakpoint
 CREATE TABLE "tracking_histories" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"document_id" uuid NOT NULL,
 	"status" "doc_status" NOT NULL,
-	"description" text,
-	"changed_by" varchar(255) NOT NULL,
+	"notes" text,
+	"changed_by" uuid NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
@@ -32,5 +33,6 @@ CREATE TABLE "users" (
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
 --> statement-breakpoint
-ALTER TABLE "documents" ADD CONSTRAINT "documents_staff_id_users_id_fk" FOREIGN KEY ("staff_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "tracking_histories" ADD CONSTRAINT "tracking_histories_document_id_documents_id_fk" FOREIGN KEY ("document_id") REFERENCES "public"."documents"("id") ON DELETE cascade ON UPDATE no action;
+ALTER TABLE "documents" ADD CONSTRAINT "documents_created_by_users_id_fk" FOREIGN KEY ("created_by") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "tracking_histories" ADD CONSTRAINT "tracking_histories_document_id_documents_id_fk" FOREIGN KEY ("document_id") REFERENCES "public"."documents"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "tracking_histories" ADD CONSTRAINT "tracking_histories_changed_by_users_id_fk" FOREIGN KEY ("changed_by") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
